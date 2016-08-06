00ae4f5183cd63a1599f089a7cb0de928c23d7c4
Cory Sabol <sible777@gmail.com>
2016-08-05 23:55:06 -0400

    Create shell of Display.h

--- Costanza_v2/Unhingen/Display/Display.h
+++ Costanza_v2/Unhingen/Display/Display.h
@@ -1,1 +1,36 @@
 #pragma once
+
+// Check if win or linux to handle paths
+#ifdef __linux__
+
+#include <SDL2/SDL.h>
+#include "../Unhingen/Common/Types.h"
+
+#else
+
+#include <SDL2\SDL.h>
+#include "..\Unhingen\Commin\Types.h"
+
+#endif
+
+/* *
+ * Display attempts to provide an abstracted interface to SDL2 and OpenGL
+ * to allow easy creation of SLD windows and OpenGL contexts.
+ *
+ * Author: Cory Sabol - 2016
+ * */
+
+class Display {
+    private:
+        SDL_Window  *window;
+        SDL_Surface *surface;
+
+    public:
+        Display (String title,
+                int SDL_WINDOWPOS_X,
+                int SDL_WINDOWPOS_Y,
+                int WINDOW_WIDTH,
+                int WINDOW_HEIGHT) {
+
+        }
+}
